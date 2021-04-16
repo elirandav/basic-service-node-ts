fc6a9c80d16153e1e3ae2706ce8e8395b548bc6c
elirandav <elirandav@gmail.com>
2021-04-16 11:07:21 +0300

    rename to expressApp

--- app.ts
+++ app.ts
@@ -3,16 +3,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var usersRouter = require('./routes/routes');
-const app = express();
+const expressApp = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('', usersRouter);
-app.use(require('body-parser').json())
+expressApp.set('views', path.join(__dirname, 'views'));
+expressApp.set('view engine', 'jade');
+expressApp.use(logger('dev'));
+expressApp.use(express.json());
+expressApp.use(express.urlencoded({ extended: false }));
+expressApp.use(cookieParser());
+expressApp.use(express.static(path.join(__dirname, 'public')));
+expressApp.use('', usersRouter);
+expressApp.use(require('body-parser').json())
 
-module.exports = app;
+module.exports = expressApp;
